d283ac628dc2a5e5672fb672bb880bc9f49d2c42
unknown <dgloria.web@gmail.com>
2023-07-22 18:23:33 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://dbgtr.netlify.app/', 'http://localhost:5173/'],
+    'allowed_origins' => ['dbgtr.netlify.app/*','dbgtr.netlify.app','localhost:5173','localhost:5173/*'],
 
     'allowed_origins_patterns' => [],
 
