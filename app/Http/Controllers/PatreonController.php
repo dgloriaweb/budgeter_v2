ae26e7804770e465d54972bb9b21987539d69300
unknown <dgloria.web@gmail.com>
2023-09-26 21:26:00 +0100

    url code

--- app/Http/Controllers/PatreonController.php
+++ app/Http/Controllers/PatreonController.php
@@ -30,12 +30,13 @@ class PatreonController extends Controller
             &client_secret= env("PATREON_CLIENT_SECRET")
             &redirect_uri=env("PATREON_REDIRECT_URI") */
 
-            $response = Http::post("www.patreon.com/api/oauth2/token", [
-                "grant_type" => "authorization_code",
-                "client_id" => env("PATREON_CLIENT_ID"),
-                "client_secret" => env("PATREON_CLIENT_SECRET"),
-                "redirect_uri" => env("PATREON_REDIRECT_URI"),
-            ]);
+        $response = Http::post("www.patreon.com/api/oauth2/token", [
+            "code" => $code,
+            "grant_type" => "authorization_code",
+            "client_id" => env("PATREON_CLIENT_ID"),
+            "client_secret" => env("PATREON_CLIENT_SECRET"),
+            "redirect_uri" => env("PATREON_REDIRECT_URI"),
+        ]);
 
         return $response;
     }
