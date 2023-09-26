dda447bb77d349a0e9c01972fe4be9be23b5e710
unknown <dgloria.web@gmail.com>
2023-09-26 20:30:29 +0100

    url code

--- routes/api.php
+++ routes/api.php
@@ -98,7 +98,7 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     });
 
     //patreon api
-    Route::get('/patreon', 'App\Http\Controllers\PatreonController@index')->name('patreongetcode');
+    Route::get('/patreon', 'App\Http\Controllers\PatreonController@index');
 
 });
 Route::middleware('auth:api')->group(function () {
