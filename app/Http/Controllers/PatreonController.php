7a0934b4b1e74af930b273a92d4a6b35035ee4cf
unknown <dgloria.web@gmail.com>
2023-09-26 20:26:14 +0100

    url code

--- app/Http/Controllers/PatreonController.php
+++ app/Http/Controllers/PatreonController.php
@@ -9,7 +9,7 @@ class PatreonController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
