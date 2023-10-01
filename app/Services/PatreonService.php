ae97093f82ab4c3e26ce0942ad5aee3d58e339dc
unknown <dgloria.web@gmail.com>
2023-10-01 18:50:55 +0100

    handling wrong token exception

--- app/Services/PatreonService.php
+++ app/Services/PatreonService.php
@@ -5,23 +5,18 @@ namespace App\Services;
 use App\Models\Patreon;
 use Exception;
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class PatreonService
 {
     protected $patreon;
 
-
-    public function getPatrons()
-    {
-        try {
-            // connect to the api, and store the data in the database
-            $response = Http::withToken(env('PATREON_TEMP_BEARER'))->get("https://www.patreon.com/api/oauth2/api/campaigns/11120253/pledges?include=patron.null");
-            return $response->json();
-        } catch (Exception $e) {
-            if ($e instanceof HttpException && $e->getStatusCode() == 401) {
-                dd($e->getMessage());
-            }
+ 
+    public function getPatrons(){
+        // connect to the api, and store the data in the database
+        $response = Http::withToken(env('PATREON_TEMP_BEARER'))->get("https://www.patreon.com/api/oauth2/api/campaigns/11120253/pledges?include=patron.null");
+        if($response->json()['errors']){
+            dd($response->json()['errors']);
         }
+        return $response->json();
     }
 }
