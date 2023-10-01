25ca7165193c9e77a8901b269efc4dedd1207386
unknown <dgloria.web@gmail.com>
2023-10-01 15:16:25 +0100

    tries

--- app/Services/PatreonService.php
+++ app/Services/PatreonService.php
@@ -13,7 +13,6 @@ class PatreonService
  
     public function getPatrons(){
         // connect to the api, and store the data in the database
-        dd(getenv('PATREON_TEMP_BEARER'));
         $response = Http::withToken(env('PATREON_TEMP_BEARER'))->get("https://www.patreon.com/api/oauth2/api/campaigns/11120253/pledges?include=patron.null");
         return $response;
     }
