ecea8537559571f50fef258cd77554e7cc680783
unknown <dgloria.web@gmail.com>
2024-04-25 15:23:30 +0100

    added enabled field to partners table

--- database/migrations/2023_02_18_171402_create_partners_table.php
+++ database/migrations/2023_02_18_171402_create_partners_table.php
@@ -16,6 +16,7 @@ class CreatePartnersTable extends Migration
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
             $table->string('partner');
+            $table->tinyInteger('enabled')->default(1);
             $table->timestamps();
         });
     }
