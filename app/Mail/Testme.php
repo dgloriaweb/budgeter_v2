7c31683bf7bcca6186922c8acd709d01b534318f
unknown <dgloria.web@gmail.com>
2024-06-06 10:34:25 +0100

    key or no key

---
+++ app/Mail/Testme.php
@@ -0,0 +1,13 @@
+<?php
+
+// use App\Mail\TestEmail;
+// use Illuminate\Support\Facades\Mail;
+
+// public function Testme(){
+
+//     Mail::to('recipient@domain.com')
+//     ->cc('cc@domain.com')
+//     ->bcc('bcc@domain.com')
+//     ->send(new TestEmail());
+    
+// } 
